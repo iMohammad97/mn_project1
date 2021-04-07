b7787c8b2b1c3a95909ef2b8c9e2059cde846847
iMohammad97 <mohammadm97i@gmail.com>
2021-04-07 20:31:02 +0430

    Initial commit: adds phase one of project.

---
+++ Src/components/components/darkColors.js
@@ -0,0 +1,7 @@
+
+export default {
+    black: '#1E1E2C', //Black Pearl
+    blue: '#2D2D44', //Black Blueberry
+    light: '#33E1ED', //Turquoise
+    white: '#FFFFFF', //White
+};
---
+++ Src/components/index.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+import Header from './components/Header';
+import LearnMoreLinks from './components/LearnMoreLinks';
+import darkColors from './components/darkColors';
+import Colors from './components/Colors';
+import DebugInstructions from './components/DebugInstructions';
+import ReloadInstructions from './components/ReloadInstructions';
+
+export {Header, LearnMoreLinks, darkColors, DebugInstructions, ReloadInstructions, Colors};
